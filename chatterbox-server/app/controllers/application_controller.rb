5b0e433e3576038705b4a6a3361676c2296b94ff
Will Lovell <willard.lovell@icloud.com>
2022-10-22 15:02:29 -0600

    Completed lab server side

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    messages = Message.created_at
+    messages.to_json
+  end
   
+post '/messages' do
+  messages = Message.create(body:params[:body], username:params[:username])
+  messages.to_json
+end
+
+patch '/messages/:id' do  
+  messages = Message.find(params[:id])
+  messages.update(body:params[:body])
+  messages.to_json
+end
+
+delete '/messages/:id' do
+  messages = Message.destroy(params[:id])
+  messages.to_json
+end
 end
